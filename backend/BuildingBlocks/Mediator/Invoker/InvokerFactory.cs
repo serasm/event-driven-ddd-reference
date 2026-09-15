e8a1ab132555bf7b98c8b7a7f45f8cd8af7353de
Serasm <smigiel.sebastian@gmail.com>
2026-09-15 22:07:03 +0200

    Cleanup

--- backend/BuildingBlocks/Mediator/Invoker/InvokerFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace Mediator.Invoker;
-
-public class InvokerFactory : IInvokerFactory
-{
-    public Delegate CreateHandler(Type queryType, Type handlerType)
-    {
-        throw new NotImplementedException();
-    }
-}
--- backend/BuildingBlocks/Mediator/Mediator.cs
+++ backend/BuildingBlocks/Mediator/Mediator.cs
@@ -1,5 +1,5 @@
 using System.Collections.Concurrent;
-using Mediator.Helpers;
+using System.Linq.Expressions;
 using Mediator.Pipelines;
 using Mediator.Requests;
 
@@ -22,7 +22,7 @@ public class Mediator : IMediator
     public Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> query, CancellationToken cancellationToken = default)
     {
         var requestType = query.GetType();
-        var (handlerType, invokerObj) = _requestHandlerInvokers.GetOrAdd(requestType, HelperInfoBuilder.BuildRequestHandlerInfo<TResponse>);
+        var (handlerType, invokerObj) = _requestHandlerInvokers.GetOrAdd(requestType, BuildRequestHandlerInfo<TResponse>);
         
         var invoker = (Func<object, IRequest<TResponse>, CancellationToken, Task<TResponse>>)invokerObj;
         
@@ -34,5 +34,28 @@ public class Mediator : IMediator
                 requestType, invoker);
         
         return pipelineInvoker(handler, query, cancellationToken);
+    }
+    
+    private static (Type QueryHandlerType, Delegate Invoker) BuildRequestHandlerInfo<TResponse>(Type requestType)
+    {
+        var responseType = typeof(TResponse);
+        var handlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, responseType);
+        var handleMethod = handlerType.GetMethod("HandleAsync") 
+                           ?? throw new InvalidOperationException($"No handle method registered for type {handlerType.Name}");
+        
+        var handlerParam = Expression.Parameter(typeof(object), "handler");
+        var requestParam = Expression.Parameter(typeof(IRequest<TResponse>), "request");
+        var ctParam = Expression.Parameter(typeof(CancellationToken), "cancellationToken");
+
+        var body = Expression.Call(
+            Expression.Convert(handlerParam, handlerType),
+            handleMethod,
+            Expression.Convert(requestParam, requestType),
+            ctParam);
+        
+        var lambda = Expression.Lambda<Func<object, IRequest<TResponse>, CancellationToken, Task<TResponse>>>(
+            body, handlerParam, requestParam, ctParam);
+
+        return (handlerType, lambda.Compile());
     }
 }
